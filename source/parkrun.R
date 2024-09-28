6577252bb64ee05629d3466f47a910adfe840f0a
mhurk <149044252+mhurk@users.noreply.github.com>
2024-09-28 10:10:30 +0200

    cleanup

--- source/parkrun.R
+++ source/parkrun.R
@@ -10,14 +10,20 @@ library(stringr)
 parkruns <- read.csv("http://www.rhoff.org.uk/Full_5k_parkrun_List.csv", sep = ",")  #download from website of Roderick Hoffman
 parkrun <- parkruns[parkruns$Country.Name == "Netherlands",]
 
-NLD <- readRDS("gadm36_NLD_1_sp.rds")
+NLD <- readRDS("./data/gadm36_NLD_1_sp.rds")
 NLD_fixed <- subset(NLD, !NLD$NAME_1  %in% c("Zeeuwse meren", "IJsselmeer"))
 outl <- raster::aggregate(NLD)
 
 ## Adding unofficial parkruns (not in the dowloaded list early January 2024)
 
-parkrun_zuiderpark <- c("eventname" = "Zuiderpark, Hoogeveen", "Longitude" = 6.4835483, "Latitude" = 52.7109916, "Status" = "Planned")
-parkrun_enschede <- c("eventname" = "Enschede", "Longitude" = 6.836273, "Latitude" = 52.2206737, "Status" = "Planned")
+parkrun_zuiderpark <- c("eventname" = "Zuiderpark, Hoogeveen", 
+                        "Longitude" = 6.4835483, 
+                        "Latitude" = 52.7109916, 
+                        "Status" = "Planned")
+parkrun_enschede <- c("eventname" = "Enschede", 
+                      "Longitude" = 6.836273,  
+                      "Latitude" = 52.2206737,
+                      "Status" = "Planned")
 
 parkrun2 <- data.frame(rbind(parkrun_zuiderpark, parkrun_enschede))
 parkrun2$Longitude <- as.numeric(parkrun2$Longitude)
@@ -68,12 +74,11 @@ pr1 <- ggplot(NLD_fixed) +
 
 pr1
 
-filename <- paste("pakruns_NL_", format(Sys.time(), "%Y%m%d-%H%M"), ".png", sep = "")
+filename <- paste("./data/pakruns_NL_", format(Sys.time(), "%Y%m%d-%H%M"), ".png", sep = "")
 ggsave(filename, height = 800, dpi = 100, units = "px", bg="white")
   
 
 ## Only the parkruns (official and unoffical), without country border
-
 ggplot(parkrun) +
   geom_point(aes(x=Longitude, y=Latitude)) +
   stat_voronoi(
@@ -83,6 +88,6 @@ ggplot(parkrun) +
     linewidth = .5) +
   coord_map() 
 
-ggsave("noborder.png", height = 800, dpi = 100, units = "px", bg="white")
+ggsave("./data/noborder.png", height = 800, dpi = 100, units = "px", bg="white")
 
 
